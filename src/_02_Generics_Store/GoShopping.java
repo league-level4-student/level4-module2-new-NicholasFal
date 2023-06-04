63c1ec2d43305dbc697c05f53809f57711284d70
root <root@c947ca479693>
2023-06-04 19:57:56 +0000

    I learned about Generics!

--- src/_02_Generics_Store/GoShopping.java
+++ src/_02_Generics_Store/GoShopping.java
@@ -11,26 +11,46 @@ public class GoShopping {
 	
 	public static void main(String[] args) {
 		// 2. Create a Cart object of type Candy
-		
+		//Cart<Candy> candy = new Cart();
+		//candy.add(new Candy());
+		//candy.add(new Candy());
+		//candy.add(new Candy());
+
 		// 3. Create another Cart object of type Cereal
-		
+		//Cart<Cereal> cereal = new Cart();
+		//cereal.add(new Cereal());
+		//cereal.add(new Cereal());
+		//cereal.add(new Cereal());
+		//cereal.add(new Cereal());
+		//cereal.add(new Cereal());
+
 		// 4. Add a few items to EACH cart
 		
 		
 		
 		
 		// 5. Call the showCart() method on EACH cart
-		
+		//candy.showCart();
+		//cereal.showCart();
 		
 		// 6. Run the code
 		
 		// 7. Comment out the code you have so far
 		
 		// 8. Repeat steps 2-5 for a Clothing Cart and a Toy Cart
-		
-		
-		
-		
+		Cart<Clothing> clothing = new Cart();
+		Cart<Toy> toys = new Cart();
+		clothing.add(new Clothing());
+		clothing.add(new Clothing());
+		clothing.add(new Clothing());
+		toys.add(new Toy());
+		toys.add(new Toy());
+		toys.add(new Toy());
+		toys.add(new Toy());
+		toys.add(new Toy());
+		toys.add(new Toy());
+		clothing.showCart();		toys.showCart();
+
 		// Why are there errors?
 		
 		// 9. Modify the Cart class so that this code compiles
